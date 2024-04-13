a2705725c6d5fc6e0325d849ed24ae30b591f6ac
LLLida <0adilmohammad0@gmail.com>
2024-04-14 01:00:42 +0300

    мой Фильтр калмана

--- scripts/time_series.py
+++ scripts/time_series.py
@@ -56,6 +56,63 @@ class KalmanFilter(object):
 
         return distance, pvalue
 
+class NewKalmanFilter:
+    def __init__(self, n, m, A, B, H1, H2, Q, R, x_hat, P):
+        self.n = n  # state dimension
+        self.m = m  # measurement dimension
+        self.A = A  # state transition matrix
+        self.B = B  # input control matrix
+        self.H1 = H1  # measurement matrix
+        self.H2 = H2  # measurement matrix
+        self.Q = Q  # process noise covariance matrix
+        self.R = R  # measurement noise covariance matrix
+        self.x_hat = x_hat  # state estimate
+        self.P = P  # estimate covariance matrix
+
+    def predict(self, u=0):
+        self.x_hat = np.dot(self.A, self.x_hat) + np.dot(self.B, u)
+
+        self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
+
+        return self.x_hat
+
+    def update1(self, z):
+        y = z - np.dot(self.H1, self.x_hat)
+        S = np.dot(np.dot(self.H1, self.P), self.H1.T) + self.R
+        K = np.dot(np.dot(self.P, self.H1.T), np.linalg.inv(S))
+        self.x_hat = self.x_hat + np.dot(K, y)
+        self.P = self.P - np.dot(np.dot(K, self.H1), self.P)
+
+    def update2(self, z):
+        y = z - np.dot(self.H2, self.x_hat)
+        S = np.dot(np.dot(self.H2, self.P), self.H2.T) + self.R
+        K = np.dot(np.dot(self.P, self.H2.T), np.linalg.inv(S))
+        self.x_hat = self.x_hat + np.dot(K, y)
+        self.P = self.P - np.dot(np.dot(K, self.H2), self.P)
+
+    def distance1(self, z):
+        y = z - np.dot(self.H1, self.x)
+        S = self.R + np.dot(self.H1, np.dot(self.P, self.H1.T))
+        distance = math.sqrt((y.T @ np.linalg.inv(S) @ y)[0,0])
+
+        # https://stats.stackexchange.com/questions/97408/relation-of-mahalanobis-distance-to-log-likelihood
+        # pvalue = 1 - chi2.cdf(100*distance, df=3)
+
+        # return distance, pvalue
+        return distance
+
+    def distance2(self, z):
+        y = z - np.dot(self.H2, self.x)
+        S = self.R + np.dot(self.H2, np.dot(self.P, self.H2.T))
+        distance = math.sqrt((y.T @ np.linalg.inv(S) @ y)[0,0])
+
+        # https://stats.stackexchange.com/questions/97408/relation-of-mahalanobis-distance-to-log-likelihood
+        # pvalue = 1 - chi2.cdf(100*distance, df=3)
+
+        # return distance, pvalue
+        return distance
+
+
 path = 'train_anomaly_5.csv'
 
 df1, df2 = pd.read_csv(path+'1'), pd.read_csv(path+'2')
@@ -66,36 +123,69 @@ print(df2.head())
 t1, y1 = df1['times'].to_numpy(), df1['P'].to_numpy()
 t2, y2 = df2['times'].to_numpy(), df2['I'].to_numpy()
 
-kalman1 = KalmanFilter(F=np.eye(2), H=np.eye(2), Q=np.array([
-    [1.0, 0],
-    [0, 0.001]
-]))
-
-kalman2 = KalmanFilter(F=np.eye(2), H=np.eye(2), Q=np.array([
-    [1.0, 0],
-    [0, 0.05]
-]), x0=np.array([[0.0], [0.17]]))
-
 fig, (ax1, ax2) = plt.subplots(nrows=2)
 ax1.plot(t1, y1)
 ax2.plot(t2, y2)
 
-kt, ky = [], []
-for t, y in zip(t1, y1):
-    z = np.array([[t], [y]], np.float32)
-    kalman1.update(z)
-    pred = kalman1.predict()
-    kt.append(pred[0, 0])
-    ky.append(pred[1, 0])
-ax1.plot(kt, ky, color='green')
+H1=np.array([
+    [1, 0, 0],
+    [0, 1, 0],
+])
+H2=np.array([
+    [1, 0, 0],
+    [0, 0, 1]
+])
+kalman = NewKalmanFilter(3, 2, A=np.eye(3), B=0, H1=H1, H2=H2, Q=np.array([
+    [1.0, 0,     0],
+    [0,   0.001, 0],
+    [0,   0,     0.05]
+]), R=np.eye(2), P=np.eye(3), x_hat=np.array([[0.0], [0.01], [0.17]]))
 
-kt, ky = [], []
-for t, y in zip(t2, y2):
-    z = np.array([[t], [y]], np.float32)
-    kalman2.update(z)
-    pred = kalman2.predict()
+kt, kx, ky = [], [], []
+dx, dy = [], []
+j = 0
+for i in range(len(t1)):
+    if j<len(t2) and t1[i] >= t2[j]:
+        z = np.array([[t2[j]], [y2[j]]])
+        kalman.update2(z)
+        j += 1
+    z = np.array([[t1[i]], [y1[i]]], np.float32)
+
+    kalman.update1(z)
+
+    pred = kalman.predict()
     kt.append(pred[0, 0])
-    ky.append(pred[1, 0])
+    kx.append(pred[1, 0])
+    ky.append(pred[2, 0])
+ax1.plot(kt, kx, color='green')
 ax2.plot(kt, ky, color='green')
+
+# kalman1 = KalmanFilter(F=np.eye(2), H=np.eye(2), Q=np.array([
+#     [1.0, 0],
+#     [0, 0.001]
+# ]))
+
+# kalman2 = KalmanFilter(F=np.eye(2), H=np.eye(2), Q=np.array([
+#     [1.0, 0],
+#     [0, 0.05]
+# ]), x0=np.array([[0.0], [0.17]]))
+
+# kt, ky = [], []
+# for t, y in zip(t1, y1):
+#     z = np.array([[t], [y]], np.float32)
+#     kalman1.update(z)
+#     pred = kalman1.predict()
+#     kt.append(pred[0, 0])
+#     ky.append(pred[1, 0])
+# ax1.plot(kt, ky, color='green')
+
+# kt, ky = [], []
+# for t, y in zip(t2, y2):
+#     z = np.array([[t], [y]], np.float32)
+#     kalman2.update(z)
+#     pred = kalman2.predict()
+#     kt.append(pred[0, 0])
+#     ky.append(pred[1, 0])
+# ax2.plot(kt, ky, color='green')
 
 plt.show()
