52929ff5b89d5a67dcee8bfd03fdb0c34d95fccb
LLLida <0adilmohammad0@gmail.com>
2024-04-14 08:29:09 +0300

    ФИнальные штрихи

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char** argv)
   SDL_Init(SDL_INIT_VIDEO);
   SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
 
-  SDL_Window* window = SDL_CreateWindow("how many bananas can fit?",
+  SDL_Window* window = SDL_CreateWindow("Hararkiri: by Primaty?",
 					SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720,
 					// SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI
 					SDL_WINDOW_ALLOW_HIGHDPI
@@ -44,6 +44,7 @@ int main(int argc, char** argv)
   ImGuiIO& io = ImGui::GetIO(); (void)io;
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;	    // тяги
 
   ImPlot::CreateContext();
 
@@ -67,7 +68,9 @@ int main(int argc, char** argv)
   bool show_demo_window = true;
   bool process_frames = true;
   bool video_done = false;
-  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  ImVec4 clear_fine_color = ImVec4(0.45f, 0.85f, 0.60f, 1.00f);
+  ImVec4 clear_anal_color = ImVec4(0.95f, 0.25f, 0.30f, 1.00f);
 
   double P_threshold = 0.012;
   double I_threshold = 0.015;
@@ -105,8 +108,14 @@ int main(int argc, char** argv)
 	    }
 	}
 
+      static bool is_analmoaly = false;
+
+      double current_second;
+
       if (process_frames) {
 	auto frame = read_frame(hui);
+	current_second = frame.timestamp;
+
 	if (frame.pData) {
 	  if (frame.type == 'I') {
 	    I_packets.add(frame.timestamp, frame.feature1);
@@ -124,8 +133,10 @@ int main(int argc, char** argv)
 	      max_i_diff.push_back(avg / count);
 	    }
 
-	    if (!max_i_diff.empty() && max_i_diff.back() > I_threshold)
+	    if (!max_i_diff.empty() && max_i_diff.back() > I_threshold) {
 	      add_anomaly(anals, frame.timestamp);
+	      is_analmoaly = true;
+	    }
 
 	  } else {
 	    P_packets.add(frame.timestamp, frame.feature1);
@@ -143,14 +154,20 @@ int main(int argc, char** argv)
 	      max_p_diff.push_back(avg / count);
 	    }
 
-	    if (!max_p_diff.empty() && max_p_diff.back() > P_threshold)
+	    if (!max_p_diff.empty() && max_p_diff.back() > P_threshold) {
 	      add_anomaly(anals, frame.timestamp);
+	      is_analmoaly = true;
+	    }
 
 	  }
 	} else {
 	  process_frames = false;
 	  video_done = true;
 	}
+      }
+
+      if (is_analmoaly && anals.back().end_second + 1.0 < current_second) {
+	is_analmoaly = false;
       }
 
       ImGui_ImplSDLRenderer2_NewFrame();
@@ -162,13 +179,13 @@ int main(int argc, char** argv)
 	ImPlot::ShowDemoWindow();
       }
 
-      ImGui::Begin("oh sexy boy");
+      ImGui::Begin("Harakiri: timeline");
       if (!video_done) {
 	ImGui::Checkbox("Processing frames enabled", &process_frames);
       } else {
 	ImGui::TextColored(ImVec4(0.1, 1.0, 0.1, 1.0), "Video is done");
       }
-      char csv_path[512];
+      static char csv_path[512];
       ImGui::InputTextWithHint("CSV path", "write CSV path you donut", csv_path, sizeof(csv_path));
       if (ImGui::Button("Save to csv")) {
 	int len = strlen(csv_path);
@@ -193,7 +210,7 @@ int main(int argc, char** argv)
 
 	ImPlot::EndPlot();
       }
-      if (ImPlot::BeginPlot("max diff")) {
+      if (ImPlot::BeginPlot("mean diff")) {
 	if (process_frames) {
 	  ImPlot::SetupAxes("time","frame size", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
 	} else {
@@ -205,15 +222,36 @@ int main(int argc, char** argv)
       }
       ImGui::End();
 
-      ImGui::Begin("Anomalies");
+      ImGui::Begin("Harakiri: anomaly list");
+      static char anal_path[512];
+      ImGui::InputTextWithHint("Anomaly path", "write txt path you donut", anal_path, sizeof(anal_path));
+      if (ImGui::Button("Save anomalies to file")) {
+	FILE* file = fopen(anal_path, "w");
+	if (file == NULL) {
+	  printf("couldn't open file %s\n!!", anal_path);
+	} else {
+	  fprintf(file, "{[\n");
+	  for (int i = 0; i< anals.size(); i++) {
+	    fprintf(file, "{ \"start\" = %.3f, \"end\" = %.3f }%c\n", anals[i].start_second, anals[i].end_second, ", "[i==anals.size()-1]);
+	  }
+	  fprintf(file, "]}\n");
+	  fclose(file);
+	}
+      }
       for (auto& anal: anals) {
-	ImGui::Text("Anomaly: #start %.3f #end %.3f", anal.start_second, anal.end_second);
+	int b = anal.start_second;
+	int e = anal.end_second;
+	ImGui::TextColored(ImVec4(0.9, 0.4, 0.3, 1.0), "Anomaly: #start %d:%d #end %d:%d", b/60, b%60, e/60, e%60 );
       }
       ImGui::End();
 
       ImGui::Render();
       SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
-      SDL_SetRenderDrawColor(renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+      if (is_analmoaly) {
+	SDL_SetRenderDrawColor(renderer, (Uint8)(clear_anal_color.x * 255), (Uint8)(clear_anal_color.y * 255), (Uint8)(clear_anal_color.z * 255), (Uint8)(clear_anal_color.w * 255));
+      } else {
+	SDL_SetRenderDrawColor(renderer, (Uint8)(clear_fine_color.x * 255), (Uint8)(clear_fine_color.y * 255), (Uint8)(clear_fine_color.z * 255), (Uint8)(clear_fine_color.w * 255));
+      }
       SDL_RenderClear(renderer);
       ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
       SDL_RenderPresent(renderer);
